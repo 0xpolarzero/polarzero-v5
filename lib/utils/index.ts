106ab4c2f930bb8a40458d3d6ab43ffda1043874
polarzero <0xpolarzero@gmail.com>
2023-11-21 11:00:59 +0100

    Remove unused components (types & utils)

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,13 +1,4 @@
-import fetchMileageLogs from './fetchMileageLogs';
-import fetchRunningLogs from './fetchRunningLogs';
 import formatValueToPrecision from './formatValueToPrecision';
-import getColormapValue from './getColormapValue';
 import validateQuery from './validateQuery';
 
-export {
-  fetchMileageLogs,
-  fetchRunningLogs,
-  formatValueToPrecision,
-  getColormapValue,
-  validateQuery,
-};
+export { formatValueToPrecision, validateQuery };
