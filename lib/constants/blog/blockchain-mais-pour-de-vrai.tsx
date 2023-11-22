b29035931cc19336cda57bab513cbde06d164413
polarzero <0xpolarzero@gmail.com>
2023-11-22 17:39:20 +0100

    Init article: La blockchain, mais pour de vrai

---
+++ lib/constants/blog/blockchain-mais-pour-de-vrai.tsx
@@ -0,0 +1,95 @@
+import { BlogPostSection } from '@/lib/types/writing';
+
+const sections: BlogPostSection[] = [
+  {
+    title: 'Présentation',
+    slug: 'landing',
+    subsections: [],
+  },
+  {
+    title: '1. Introduction : champ d’application et hypothèse',
+    slug: 'introduction-champ-d-application-et-hypothese',
+    subsections: [
+      { title: 'Quoi, et pourquoi ?', slug: 'quoi-et-pourquoi' },
+      { title: 'Biais et perception', slug: 'biais-et-perception' },
+    ],
+  },
+  {
+    title: '2. Une image profondément dégradée',
+    slug: 'une-image-profondement-degradee',
+    subsections: [
+      { title: 'NFTs', slug: 'nfts' },
+      { title: 'Cryptomonnaies', slug: 'cryptomonnaies' },
+      { title: 'Arnaques et faillites', slug: 'arnaques-et-faillites' },
+      { title: 'Impact écologique', slug: 'impact-ecologique' },
+    ],
+  },
+  {
+    title: '3. Des explications à cette réputation corrompue',
+    slug: 'des-explications-a-cette-reputation-corrompue',
+    subsections: [
+      { title: 'Couverture médiatique', slug: 'couverture-mediatic' },
+      { title: 'Enjeux économiques', slug: 'enjeux-economiques' },
+    ],
+  },
+  {
+    title: '4. Une essence véritablement positive',
+    slug: 'une-essence-veritablement-positive',
+    subsections: [
+      { title: 'Stéréotypes (partie 2)', slug: 'stereotypes-partie-2' },
+      { title: 'Philosophie : « don’t trust, verify »', slug: 'philosophie-dont-trust-verify' },
+      { title: 'Plateformes décentralisées', slug: 'plateformes-decentralisees' },
+    ],
+  },
+  {
+    title: '5. Un fonctionnement participatif et responsable',
+    slug: 'un-fonctionnement-participatif-et-responsable',
+    subsections: [
+      { title: 'Bases de données distribuées', slug: 'bases-de-donnees-distribuees' },
+      { title: 'Portefeuille numérique', slug: 'portefeuille-numerique' },
+      { title: 'Contrats intelligents', slug: 'contrats-intelligents' },
+    ],
+  },
+  {
+    title: '6. Un impact inévitable à venir',
+    slug: 'un-impact-inevitable-a-venir',
+    subsections: [
+      { title: 'Transformation numérique', slug: 'transformation-numerique' },
+      { title: 'Deux scénarios probables', slug: 'deux-scenarios-probables' },
+    ],
+  },
+  {
+    title: '7. Connaissance, lucidité et pouvoir',
+    slug: 'connaissance-lucidite-et-pouvoir',
+    subsections: [
+      { title: 'Pourquoi s’informer ?', slug: 'pourquoi-s-informer' },
+      { title: 'Pourquoi maintenant ?', slug: 'pourquoi-maintenant' },
+      { title: 'Que faire ?', slug: 'que-faire' },
+    ],
+  },
+  {
+    title: '8. Conclusion : avenir et pistes d’exploration',
+    slug: 'conclusion-avenir-et-pistes-d-exploration',
+    subsections: [
+      { title: 'Portefeuille numérique', slug: 'portefeuille-numerique-2' },
+      { title: 'Recherche et publication', slug: 'recherche-et-publication' },
+      { title: 'Gouvernance communautaire', slug: 'gouvernance-communautaire' },
+      {
+        title: 'Biens communs et financement participatif',
+        slug: 'biens-communs-et-financement-participatif',
+      },
+      { title: 'Musique', slug: 'musique' },
+      { title: 'Arts et culture', slug: 'arts-et-culture' },
+      { title: 'Réseaux sociaux', slug: 'reseaux-sociaux' },
+      { title: 'Développement', slug: 'developpement' },
+      { title: 'Autres', slug: 'autres' },
+    ],
+  },
+  {
+    title: '9. Références',
+    slug: 'references',
+    subsections: [],
+  },
+];
+
+export default sections;
--- lib/constants/blog/index.tsx
+++ lib/constants/blog/index.tsx
@@ -1,7 +1,9 @@
 import BLOCKCHAIN_BUT_FOR_REAL from './blockchain-but-for-real';
+import BLOCKCHAIN_MAIS_POUR_DE_VRAI from './blockchain-mais-pour-de-vrai';
 
 import { BlogPostSection } from '@/lib/types/writing';
 
 export const SECTIONS: Record<string, BlogPostSection[]> = {
   'blockchain-but-for-real': BLOCKCHAIN_BUT_FOR_REAL,
+  'blockchain-mais-pour-de-vrai': BLOCKCHAIN_MAIS_POUR_DE_VRAI,
 };
