cf2af3f8e5b6fd989fa6603187ef41038d862679
polarzero <0xpolarzero@gmail.com>
2023-11-23 12:05:25 +0100

    Add article: Chainlink's new dawn

---
+++ lib/constants/blog/chainlink-new-dawn.tsx
@@ -0,0 +1,41 @@
+import { BlogPostSection } from '@/lib/types/writing';
+
+const sections: BlogPostSection[] = [
+  {
+    title: 'Overview',
+    slug: '',
+    subsections: [],
+  },
+  {
+    title: '0. TLDR;',
+    slug: '0-tldr-',
+    subsections: [],
+  },
+  {
+    title: '1. CCIP: The cornerstone for financial institutions',
+    slug: '1-ccip-the-cornerstone-for-financial-institutions',
+    subsections: [],
+  },
+  {
+    title: '2. The backbone of Web3',
+    slug: '2-the-backbone-of-web3',
+    subsections: [],
+  },
+  {
+    title: '3. Functions, or how to actually bridge the gap',
+    slug: '3-functions-or-how-to-actually-bridge-the-gap',
+    subsections: [],
+  },
+  {
+    title: '4. Closing thoughts',
+    slug: '4-closing-thoughts',
+    subsections: [],
+  },
+  {
+    title: '5. References',
+    slug: '5-references',
+    subsections: [],
+  },
+];
+
+export default sections;
--- lib/constants/blog/index.tsx
+++ lib/constants/blog/index.tsx
@@ -1,10 +1,12 @@
 import BLOCKCHAIN_BUT_FOR_REAL from './blockchain-but-for-real';
 import BLOCKCHAIN_MAIS_POUR_DE_VRAI from './blockchain-mais-pour-de-vrai';
+import CHAINLINK_NEW_DAWN from './chainlink-new-dawn';
 import DECENTRALIZED_SYSTEMS_END_THE_CYCLE_OF_INDIFFERENCE from './decentralized-systems-end-the-cycle-of-indifference';
 
 import { BlogPostSection } from '@/lib/types/writing';
 
 export const SECTIONS: Record<string, BlogPostSection[]> = {
+  'chainlink-new-dawn': CHAINLINK_NEW_DAWN,
   'blockchain-but-for-real': BLOCKCHAIN_BUT_FOR_REAL,
   'blockchain-mais-pour-de-vrai': BLOCKCHAIN_MAIS_POUR_DE_VRAI,
   'decentralized-systems-end-the-cycle-of-indifference':
