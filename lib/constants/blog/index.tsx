e63102d63c48c83a38b285c1701c80a4208cf939
polarzero <0xpolarzero@gmail.com>
2023-11-23 16:35:36 +0100

    Add article: What is the metaverse anyway

--- lib/constants/blog/index.tsx
+++ lib/constants/blog/index.tsx
@@ -4,16 +4,18 @@ import CHAINLINK_NEW_DAWN from './chainlink-new-dawn';
 import DECENTRALIZED_SYSTEMS_END_THE_CYCLE_OF_INDIFFERENCE from './decentralized-systems-end-the-cycle-of-indifference';
 import LESSON_0_FUNDAMENTALS_OF_SOLIDITY_STORAGE from './lesson-0-fundamentals-of-solidity-storage';
 import SMART_CONTRACT_SECURITY_TERMINOLOGY_OF_A_REVIEW from './smart-contract-security-terminology-of-a-review';
+import WHAT_IS_THE_METAVERSE_ANYWAY from './what-is-the-metaverse-anyway';
 
 import { BlogPostSection } from '@/lib/types/writing';
 
 export const SECTIONS: Record<string, BlogPostSection[]> = {
+  'blockchain-but-for-real': BLOCKCHAIN_BUT_FOR_REAL,
+  'blockchain-mais-pour-de-vrai': BLOCKCHAIN_MAIS_POUR_DE_VRAI,
+  'chainlink-new-dawn': CHAINLINK_NEW_DAWN,
+  'decentralized-systems-end-the-cycle-of-indifference':
+    DECENTRALIZED_SYSTEMS_END_THE_CYCLE_OF_INDIFFERENCE,
   'lesson-0-fundamentals-of-solidity-storage': LESSON_0_FUNDAMENTALS_OF_SOLIDITY_STORAGE,
   'smart-contract-security-terminology-of-a-review':
     SMART_CONTRACT_SECURITY_TERMINOLOGY_OF_A_REVIEW,
-  'chainlink-new-dawn': CHAINLINK_NEW_DAWN,
-  'blockchain-but-for-real': BLOCKCHAIN_BUT_FOR_REAL,
-  'blockchain-mais-pour-de-vrai': BLOCKCHAIN_MAIS_POUR_DE_VRAI,
-  'decentralized-systems-end-the-cycle-of-indifference':
-    DECENTRALIZED_SYSTEMS_END_THE_CYCLE_OF_INDIFFERENCE,
+  'what-is-the-metaverse-anyway': WHAT_IS_THE_METAVERSE_ANYWAY,
 };
---
+++ lib/constants/blog/what-is-the-metaverse-anyway.tsx
@@ -0,0 +1,63 @@
+import { BlogPostSection } from '@/lib/types/writing';
+
+const sections: BlogPostSection[] = [
+  {
+    title: 'Introduction',
+    slug: '',
+    subsections: [],
+  },
+  {
+    title: 'Origins and Web3',
+    slug: 'origins-and-web3',
+    subsections: [
+      { title: 'Where does this term come from?', slug: 'where-does-this-term-come-from-' },
+      {
+        title: 'Oh yes, decentralization! Welcome Web3!',
+        slug: 'oh-yes-decentralization-welcome-web3-',
+      },
+    ],
+  },
+  {
+    title: 'Beyond virtual worlds',
+    slug: 'beyond-virtual-worlds',
+    subsections: [
+      { title: 'So, what is a metaverse, then?', slug: 'so-what-is-a-metaverse-then-' },
+      {
+        title: "Don't they look more like video games?",
+        slug: 'don-t-they-look-more-like-video-games-',
+      },
+    ],
+  },
+  {
+    title: 'Impact on youth and society',
+    slug: 'impact-on-youth-and-society',
+    subsections: [
+      {
+        title:
+          "But, given that link with video games, shouldn't this especially affect young people?",
+        slug: 'but-given-that-link-with-video-games-shouldn-t-this-especially-affect-young-people-',
+      },
+      {
+        title: 'I get it! The metaverse actually _already_ exists then.',
+        slug: 'i-get-it-the-metaverse-actually-already-exists-then-',
+      },
+    ],
+  },
+  {
+    title: 'Blurring lines with reality',
+    slug: 'blurring-lines-with-reality',
+    subsections: [
+      {
+        title:
+          "So, the metaverse is not a video game, it doesn't exist yet either... What is it, then?",
+        slug: 'so-the-metaverse-is-not-a-video-game-it-doesn-t-exist-yet-either-what-is-it-then-',
+      },
+      {
+        title: 'Great! I hope I will be able to join the metaverse(s)!',
+        slug: 'great-i-hope-i-will-be-able-to-join-the-metaverse-s-',
+      },
+    ],
+  },
+];
+
+export default sections;
