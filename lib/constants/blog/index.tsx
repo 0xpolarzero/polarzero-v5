682cbcb6ec61123da7ce21b1023c7543ff934e09
polarzero <0xpolarzero@gmail.com>
2023-11-23 12:32:44 +0100

    Add article: Smart contract security, terminology of a review

--- lib/constants/blog/index.tsx
+++ lib/constants/blog/index.tsx
@@ -2,10 +2,13 @@ import BLOCKCHAIN_BUT_FOR_REAL from './blockchain-but-for-real';
 import BLOCKCHAIN_MAIS_POUR_DE_VRAI from './blockchain-mais-pour-de-vrai';
 import CHAINLINK_NEW_DAWN from './chainlink-new-dawn';
 import DECENTRALIZED_SYSTEMS_END_THE_CYCLE_OF_INDIFFERENCE from './decentralized-systems-end-the-cycle-of-indifference';
+import SMART_CONTRACT_SECURITY_TERMINOLOGY_OF_A_REVIEW from './smart-contract-security-terminology-of-a-review';
 
 import { BlogPostSection } from '@/lib/types/writing';
 
 export const SECTIONS: Record<string, BlogPostSection[]> = {
+  'smart-contract-security-terminology-of-a-review':
+    SMART_CONTRACT_SECURITY_TERMINOLOGY_OF_A_REVIEW,
   'chainlink-new-dawn': CHAINLINK_NEW_DAWN,
   'blockchain-but-for-real': BLOCKCHAIN_BUT_FOR_REAL,
   'blockchain-mais-pour-de-vrai': BLOCKCHAIN_MAIS_POUR_DE_VRAI,
---
+++ lib/constants/blog/smart-contract-security-terminology-of-a-review.tsx
@@ -0,0 +1,53 @@
+import { BlogPostSection } from '@/lib/types/writing';
+
+const sections: BlogPostSection[] = [
+  {
+    title: 'Overview',
+    slug: '',
+    subsections: [],
+  },
+  {
+    title: '1. The Power of Words: “Security Reviews” vs. “Security Audits”',
+    slug: '1-the-power-of-words-security-reviews-vs-security-audits-',
+    subsections: [
+      {
+        title: 'Why “Security Reviews” Resonate Better',
+        slug: 'why-security-reviews-resonate-better',
+      },
+      { title: 'The Cultural Shift', slug: 'the-cultural-shift' },
+    ],
+  },
+  {
+    title: '2. The Toolbox: Understanding Key Techniques',
+    slug: '2-the-toolbox-understanding-key-techniques',
+    subsections: [
+      {
+        title: 'Static Analysis: Preliminary Code Assessment',
+        slug: 'static-analysis-preliminary-code-assessment',
+      },
+      { title: 'Fuzzing: Random Testing Techniques', slug: 'fuzzing-random-testing-techniques' },
+      {
+        title:
+          'Symbolic Execution and Formal Verification: Mathematical Approaches to Code Validation',
+        slug: 'symbolic-execution-and-formal-verification-mathematical-approaches-to-code-validation',
+      },
+      {
+        title: 'The FREI-PI Pattern: A Holistic Perspective on Smart Contract Integrity',
+        slug: 'the-frei-pi-pattern-a-holistic-perspective-on-smart-contract-integrity',
+      },
+      { title: 'The Synergy of Techniques', slug: 'the-synergy-of-techniques' },
+    ],
+  },
+  {
+    title: '3. Wrapping Up',
+    slug: '3-wrapping-up',
+    subsections: [],
+  },
+  {
+    title: '4. References',
+    slug: '4-references',
+    subsections: [],
+  },
+];
+
+export default sections;
