aa8fab4c82b3d9c982f16809e27a51aca44964e2
polarzero <0xpolarzero@gmail.com>
2023-11-24 16:05:58 +0100

    Update ui from shadcn & customize className

--- components/ui/index.tsx
+++ components/ui/index.tsx
@@ -1,11 +1,30 @@
 import Badge from './badge';
 import Button from './button';
 import CodeBlock from './code-block';
+import * as Form from './form';
 import HoverCard from './hover-card';
 import IconButton from './icon-button';
+import { Input } from './input';
+import { Label } from './label';
 import Select from './select';
+import { Textarea } from './textarea';
 import Toaster from './toaster';
 import { useToast } from './toaster/useToast';
 import Tooltip from './tooltip';
 
-export { Badge, Button, CodeBlock, HoverCard, IconButton, Select, Toaster, Tooltip, useToast };
+export {
+  Badge,
+  Button,
+  CodeBlock,
+  HoverCard,
+  IconButton,
+  Select,
+  Toaster,
+  Tooltip,
+  useToast,
+  // shadcn/ui
+  Form,
+  Input,
+  Label,
+  Textarea,
+};
