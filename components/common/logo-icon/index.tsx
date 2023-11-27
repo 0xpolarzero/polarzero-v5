b6a606c20a4d35c1a1c960da79b02328531dbbad
polarzero <0xpolarzero@gmail.com>
2023-11-27 15:39:51 +0100

    Create LogoIcon component, use it for writing platforms, audit platforms & protocols

---
+++ components/common/logo-icon/index.tsx
@@ -0,0 +1,12 @@
+import { Code4renaIcon, HashnodeIcon, ImmunefiIcon, MediumIcon } from './icons';
+
+const LogoIcon = () => null;
+
+LogoIcon.Medium = MediumIcon;
+LogoIcon.Hashnode = HashnodeIcon;
+LogoIcon.Code4rena = Code4renaIcon;
+LogoIcon.Immunefi = ImmunefiIcon;
+
+LogoIcon.displayName = 'LogoIcon';
+
+export default LogoIcon;
