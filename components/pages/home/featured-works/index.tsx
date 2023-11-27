8c23ec24eb9352aa585a569db9baabae9a7372c2
polarzero <0xpolarzero@gmail.com>
2023-11-27 18:32:31 +0100

    Add featured works (audit competition & github repo)

--- components/pages/home/featured-works/index.tsx
+++ components/pages/home/featured-works/index.tsx
@@ -2,26 +2,12 @@ import type { FC } from 'react';
 
 import { FEATURED_WORKS } from '@/lib/constants/featured-works';
 
-// import BitTwiddlingFeature from './works/bit-twiddling';
-// import ChessFeature from './works/chess';
-
-// -----------------------------------------------------------------------------
-// Props
-// -----------------------------------------------------------------------------
-
-// type FeaturedWorksProps = {};
-
 // -----------------------------------------------------------------------------
 // Component
 // -----------------------------------------------------------------------------
 
 const FeaturedWorks: FC = () => {
-  return (
-    <div className="grid grid-cols-2 gap-4 min-[960px]:grid-cols-4">
-      {/* writing blockchain but for real, decentralized systems, ens audit, challenge writeup */}
-      {FEATURED_WORKS}
-    </div>
-  );
+  return <div className="grid grid-cols-2 gap-4 min-[960px]:grid-cols-4">{FEATURED_WORKS}</div>;
 };
 
 FeaturedWorks.displayName = 'FeaturedWorks';
---
+++ lib/constants/featured-works.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+
+import { PORTFOLIO_PAGES } from './portfolio';
+import { WRITING_BLOG_PAGES } from './writing';
+
+import { Audit } from '@/lib/types/portfolio';
+
+import FeaturedRepoFeature from '@/components/pages/home/featured-works/featured-repo';
+import AuditCardFeature from '@/components/pages/portfolio/audit-card';
+import WritingCardFeature from '@/components/pages/writing/writing-card';
+
+export const FEATURED_WORKS: ReactNode[] = [
+  <WritingCardFeature key={0} {...WRITING_BLOG_PAGES[0]} />,
+  <WritingCardFeature key={1} {...WRITING_BLOG_PAGES[2]} />,
+  <FeaturedRepoFeature
+    key={2}
+    name="Storage collision formal verification"
+    description="How some automated testing tools (fuzzing, FV) can fail to discover precise storage collision exploits, yet why it matters more than ever to use them."
+    url="https://github.com/0xpolarzero/storage-collision-formal-verification"
+  />,
+  <AuditCardFeature
+    key={3}
+    {...(PORTFOLIO_PAGES[0] as Audit)}
+    className="col-span-2 w-full min-[960px]:col-span-4"
+  />,
+];
