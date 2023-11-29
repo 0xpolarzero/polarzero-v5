e4b46199e086a0ff9ee448d642c758a3aaf4de97
polarzero <0xpolarzero@gmail.com>
2023-11-29 16:44:39 +0100

    Add Badger eBTC audit page

--- lib/constants/portfolio/index.tsx
+++ lib/constants/portfolio/index.tsx
@@ -1,6 +1,7 @@
 import { AUDIT_COMPETITIONS } from './audit-competitions';
 import { AUDIT_SOLOS } from './audit-solos';
 import { BUG_BOUNTIES } from './bug-bounties';
+import BADGER_EBTC_2023_10 from './sections/2023-10-badger-ebtc-audit-certora-formal-verification-competition';
 import ENS_2023_10 from './sections/2023-10-ens';
 
 import { Audit, BugBounty } from '@/lib/types/portfolio';
@@ -15,6 +16,7 @@ export const PORTFOLIO_PAGES: (Audit | BugBounty)[] = [
 
 export const SECTIONS: Record<string, BlogPostSection[]> = {
   '2023-10-ens': ENS_2023_10,
+  '2023-10-badger-ebtc-audit-certora-formal-verification-competition': BADGER_EBTC_2023_10,
 };
 
 /**
