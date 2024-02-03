e3ebdf50690ca95ad5e3045612057df31711bf04
polarzero <0xpolarzero@gmail.com>
2024-02-03 09:23:22 +0100

    Add ui components

--- components/ui/index.tsx
+++ components/ui/index.tsx
@@ -1,12 +1,14 @@
 import Badge from './badge';
 import Button from './button';
 import CodeBlock from './code-block';
+import { Dialog } from './dialog';
 import * as Form from './form';
 import HoverCard from './hover-card';
 import IconButton from './icon-button';
 import { Input } from './input';
 import { Label } from './label';
 import Select from './select';
+import { Slider } from './slider';
 import * as Table from './table';
 import { Textarea } from './textarea';
 import Toaster from './toaster';
@@ -24,9 +26,11 @@ export {
   Tooltip,
   useToast,
   // shadcn/ui
+  Dialog,
   Form,
   Input,
   Label,
+  Slider,
   Table,
   Textarea,
 };
