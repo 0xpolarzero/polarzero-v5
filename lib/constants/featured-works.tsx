84232a28e6284f17064f1015b087543fce9c8140
0xpolarzero <0xpolarzero@gmail.com>
2025-02-01 17:19:41 +0100

    feat: feature savvy

--- lib/constants/featured-works.tsx
+++ lib/constants/featured-works.tsx
@@ -5,5 +5,5 @@ export const FEATURED_WORKS: ResumeItem[] = [
   PORTFOLIO_PAGES.find((item) => item.title === 'DEX Indexer'),
   PORTFOLIO_PAGES.find((item) => item.title === 'DEX GraphQL'),
   PORTFOLIO_PAGES.find((item) => item.title === 'Reactive Tables'),
-  PORTFOLIO_PAGES.find((item) => item.title === 'Primodium'),
+  PORTFOLIO_PAGES.find((item) => item.title === 'savvy'),
 ] as ResumeItem[];
